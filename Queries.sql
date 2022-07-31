47b8694f3459dce1aaa7ce065ae9be8c40c8a6c9
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2022-07-31 18:01:07 -0400

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,1 +1,13 @@
+/* What is the maximun quantity sold in a transaction? */
 
+SELECT
+max(Quantity) as MaxQuantity, count(*) as Transactions
+FROM `robust-team-356223.SuperStore_Data.OrderDetails`
+
+/* How many unique products in transactions overall? */
+
+SELECT distinct
+ProductID, Quantity
+FROM `robust-team-356223.SuperStore_Data.OrderDetails`
+WHERE Quantity = 3
+order by ProductID asc, Quantity desc
