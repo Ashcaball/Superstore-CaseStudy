b68124b7301c68026eca2747733a72a5a0e269e2
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2022-07-31 21:11:37 -0400

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -2,7 +2,7 @@
 
 SELECT
 max(Quantity) as MaxQuantity, count(*) as Transactions
-FROM `robust-team-356223.SuperStore_Data.OrderDetails`
+FROM `robust-team-356223.SuperStore_Data.OrderDetails`;
 
 /* How many unique products in transactions overall? */
 
@@ -10,4 +10,21 @@ SELECT distinct
 ProductID, Quantity
 FROM `robust-team-356223.SuperStore_Data.OrderDetails`
 WHERE Quantity = 3
-order by ProductID asc, Quantity desc
+order by ProductID asc, Quantity desc;
+
+/* How many unique properties are there in the 'OrderDetails' table? */
+
+SELECT distinct
+PropertyID
+FROM `robust-team-356223.SuperStore_Data.OrderDetails`;
+
+-- can also be done this way
+
+SELECT 
+count(distinct(PropertyID)) AS UniqueID
+FROM `robust-team-356223.SuperStore_Data.OrderDetails`;
+
+/* Which product category has maximum products?  */
+/* Which state has most stores present? */
+/* Top 5 product ID's that did most sales in terms of quantity */
+/* Top 5 stores that did the most sales in terms of quantity */
