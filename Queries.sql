8b39da0f37fac7d8b3787da251fed1434430ae99
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2022-07-31 23:36:04 -0400

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -26,5 +26,29 @@ FROM `robust-team-356223.SuperStore_Data.OrderDetails`;
 
 /* Which product category has maximum products?  */
 /* Which state has most stores present? */
+
+SELECT PropertyState ,count(Prop_ID) as Locations
+FROM `robust-team-356223.SuperStore_Data.PropertyInfo`
+GROUP BY PropertyState
+ORDER BY Locations desc; 
+
 /* Top 5 product ID's that did most sales in terms of quantity */
+
+SELECT OrderDetails.ProductID, Products.ProductName, sum(Quantity) AS QuantitySold
+FROM `robust-team-356223.SuperStore_Data.OrderDetails` AS OrderDetails
+JOIN `robust-team-356223.SuperStore_Data.Products` AS Products
+ON OrderDetails.ProductID = Products.ProductID
+GROUP BY 1, 2
+ORDER BY 3 desc
+Limit 5; 
+
 /* Top 5 stores that did the most sales in terms of quantity */
+
+SELECT OrderDetails.PropertyID, PropertyInfo.PropertyCity, sum(Quantity) AS QuantitySold
+FROM `robust-team-356223.SuperStore_Data.OrderDetails` AS OrderDetails
+JOIN `robust-team-356223.SuperStore_Data.PropertyInfo` AS PropertyInfo
+ON OrderDetails.PropertyID = PropertyInfo.Prop_ID
+GROUP BY 1, 2
+ORDER BY 3 desc
+LIMIT 5;
+ 
