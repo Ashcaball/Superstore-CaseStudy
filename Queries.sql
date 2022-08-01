e461ba45532cfc8340f4e1653398b6d41f686273
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2022-08-01 00:10:24 -0400

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -40,13 +40,13 @@ ORDER BY Locations desc;
 
 /* Top 5 product ID's that did most sales in terms of quantity */
 
-SELECT OrderDetails.ProductID, Products.ProductName, sum(Quantity) AS QuantitySold
+SELECT OrderDetails.ProductID, Products.ProductName, sum(Quantity) AS QuantitySold, sum(Quantity*Price) AS Sales
 FROM `robust-team-356223.SuperStore_Data.OrderDetails` AS OrderDetails
 JOIN `robust-team-356223.SuperStore_Data.Products` AS Products
 ON OrderDetails.ProductID = Products.ProductID
 GROUP BY 1, 2
 ORDER BY 3 desc
-Limit 5; 
+Limit 5;
 
 /* Top 5 stores that did the most sales in terms of quantity */
 
@@ -57,4 +57,16 @@ ON OrderDetails.PropertyID = PropertyInfo.Prop_ID
 GROUP BY 1, 2
 ORDER BY 3 desc
 LIMIT 5;
- 
+
+/* Which city has done the most sales? */
+
+SELECT
+ pi.PropertyCity, sum(O.Quantity*P.Price) AS Sales
+FROM `robust-team-356223.SuperStore_Data.OrderDetails` AS O
+left join `robust-team-356223.SuperStore_Data.Products`  as P 
+  on O.ProductID = P.ProductID
+left join `robust-team-356223.SuperStore_Data.PropertyInfo` as pi 
+  on O.PropertyID = pi.Prop_ID
+group by  pi.PropertyCity
+order by Sales desc
+LIMIT 5;
