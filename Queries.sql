6c2adf8051f9b64e095c905f43c9af9706ab51c3
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2023-01-27 17:58:11 -0500

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -6,7 +6,7 @@ SELECT
 max(Quantity) as MaxQuantity, count(*) as Transactions
 FROM `robust-team-356223.SuperStore_Data.OrderDetails`;
 
-Result: Quantity of 3 sold in 5000 transactions
+-- Result: Quantity of 3 sold in 5000 transactions
 
 /* How many unique products in transactions overall? */
 
@@ -16,7 +16,7 @@ FROM `robust-team-356223.SuperStore_Data.OrderDetails`
 WHERE Quantity = 3
 order by ProductID asc, Quantity desc;
 
-Result: 50 unique products solds. 3 transaction for each.
+-- Result: 50 unique products solds. 3 transaction for each.
 
 /* How many unique properties are there in the 'OrderDetails' table? */
 
@@ -24,7 +24,7 @@ SELECT distinct
 PropertyID
 FROM `robust-team-356223.SuperStore_Data.OrderDetails`;
 
-Result: 20 unique locations.
+-- Result: 20 unique locations.
 
 -- can also be done this way
 
@@ -39,7 +39,7 @@ FROM `robust-team-356223.SuperStore_Data.Products`
 GROUP BY 1
 ORDER BY 2 desc;
 
-Result: Product categories include: Furnishings, Public areas, Housekeeping, Maintenance, and Office supplies. Furnishings has most products.  
+-- Result: Product categories include: Furnishings, Public areas, Housekeeping, Maintenance, and Office supplies. Furnishings has most products.  
 
 /* Which state has most stores present? */
 
@@ -48,7 +48,7 @@ FROM `robust-team-356223.SuperStore_Data.PropertyInfo`
 GROUP BY PropertyState
 ORDER BY Locations desc; 
 
-Result: Virginia and California are tied with having 2 store locations each.
+-- Result: Virginia and California are tied with having 2 store locations each.
 
 /* Top 5 product ID's that did most sales in terms of quantity */
 
@@ -60,7 +60,7 @@ GROUP BY 1, 2
 ORDER BY 3 desc
 Limit 5;
 
-Result: Products with most sales include small area rug, bed sheet (king), mirror, portable drill, luggage cart.
+-- Result: Products with most sales include small area rug, bed sheet (king), mirror, portable drill, luggage cart.
 
 /* Top 5 stores that did the most sales in terms of quantity */
 
@@ -72,7 +72,7 @@ GROUP BY 1, 2
 ORDER BY 3 desc
 LIMIT 5;
 
-Result: 1. Philadelphia 551, 2. Kansas City 545, 3. Chicago 539, 4. Cincinnati 526, 5. Las Vegas 526
+-- Result: 1. Philadelphia 551, 2. Kansas City 545, 3. Chicago 539, 4. Cincinnati 526, 5. Las Vegas 526
 
 /* Which city has done the most sales? */
 
@@ -87,7 +87,7 @@ group by  pi.PropertyCity
 order by Sales desc
 LIMIT 5;
 
-Result: 1. Kansas City, 2. Chicago, 3. Los Angeles, 4. New Orleans, 5. Boston
+-- Result: 1. Kansas City, 2. Chicago, 3. Los Angeles, 4. New Orleans, 5. Boston
 
 /* Top 5 Products sold in the Arlington location? */ 
 
@@ -104,4 +104,4 @@ group by pi.PropertyCity, p.ProductName
 order by Sales desc
 LIMIT 5;
 
-Result: 1. Sofa, 2. Chest of Drawers 3. Armoire 4. Coffe Maker 5. Portable Drill
+-- Result: 1. Sofa, 2. Chest of Drawers 3. Armoire 4. Coffe Maker 5. Portable Drill
