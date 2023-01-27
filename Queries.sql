abe5392d0b91356c4461fe537e7556296262df0b
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2023-01-27 17:44:42 -0500

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,8 +1,12 @@
+This is a sample dataset.
+
 /* What is the maximun quantity sold in a transaction? */
 
 SELECT
 max(Quantity) as MaxQuantity, count(*) as Transactions
 FROM `robust-team-356223.SuperStore_Data.OrderDetails`;
+
+Result: Quantity of 3 sold in 5000 transactions
 
 /* How many unique products in transactions overall? */
 
@@ -12,11 +16,15 @@ FROM `robust-team-356223.SuperStore_Data.OrderDetails`
 WHERE Quantity = 3
 order by ProductID asc, Quantity desc;
 
+Result: 50 unique products solds. 3 transaction for each.
+
 /* How many unique properties are there in the 'OrderDetails' table? */
 
 SELECT distinct
 PropertyID
 FROM `robust-team-356223.SuperStore_Data.OrderDetails`;
+
+Result: 20 unique locations.
 
 -- can also be done this way
 
@@ -31,12 +39,16 @@ FROM `robust-team-356223.SuperStore_Data.Products`
 GROUP BY 1
 ORDER BY 2 desc;
 
+Result: Product categories include: Furnishings, Public areas, Housekeeping, Maintenance, and Office supplies. Furnishings has most products.  
+
 /* Which state has most stores present? */
 
 SELECT PropertyState ,count(Prop_ID) as Locations
 FROM `robust-team-356223.SuperStore_Data.PropertyInfo`
 GROUP BY PropertyState
 ORDER BY Locations desc; 
+
+Result: Virginia and California are tied with having 2 store locations each.
 
 /* Top 5 product ID's that did most sales in terms of quantity */
 
@@ -48,6 +60,8 @@ GROUP BY 1, 2
 ORDER BY 3 desc
 Limit 5;
 
+Result: Products with most sales include small area rug, bed sheet (king), mirror, portable drill, luggage cart.
+
 /* Top 5 stores that did the most sales in terms of quantity */
 
 SELECT OrderDetails.PropertyID, PropertyInfo.PropertyCity, sum(Quantity) AS QuantitySold
@@ -57,6 +71,8 @@ ON OrderDetails.PropertyID = PropertyInfo.Prop_ID
 GROUP BY 1, 2
 ORDER BY 3 desc
 LIMIT 5;
+
+Result: 1. Philadelphia 551, 2. Kansas City 545, 3. Chicago 539, 4. Cincinnati 526, 5. Las Vegas 526
 
 /* Which city has done the most sales? */
 
@@ -70,6 +86,8 @@ left join `robust-team-356223.SuperStore_Data.PropertyInfo` as pi
 group by  pi.PropertyCity
 order by Sales desc
 LIMIT 5;
+
+Result: 1. Kansas City, 2. Chicago, 3. Los Angeles, 4. New Orleans, 5. Boston
 
 /* Top 5 Products sold at each store */ 
 
